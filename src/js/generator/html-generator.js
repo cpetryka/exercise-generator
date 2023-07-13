be4272f2010cf6b29d395507a431522d8795997a
Cezary Petryka <cezaryp10@gmail.com>
2023-07-13 19:05:39 +0200

    add comments in HtmlGenerator

--- src/js/generator/html-generator.js
+++ src/js/generator/html-generator.js
@@ -4,12 +4,14 @@ class HtmlGenerator {
 
         const allowedAttributes = ['id', 'class', 'src', 'alt', 'data-src', 'data-bs-toggle', 'data-bs-target', 'aria-expanded', 'aria-controls', 'aria-labelledby', 'data-bs-parent' ];
 
-        for(let key in attributes) {
-            if(allowedAttributes.includes(key)) {
-                div.setAttribute(key, attributes[key]);
+        // Setting attributes
+        for(let attribute in attributes) {
+            if(allowedAttributes.includes(attribute)) {
+                div.setAttribute(attribute, attributes[attribute]);
             }
         }
 
+        // Setting text and/or html
         if(attributes.text !== undefined && attributes.text.length) {
             div.innerText = attributes.text;
         }
