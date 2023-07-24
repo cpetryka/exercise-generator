364763a899698daa189a76c0fc5d70f936988eca
Cezary Petryka <cezaryp10@gmail.com>
2023-07-24 15:08:29 +0200

    HtmlGenerator code refactorisation

--- src/js/generator/html-generator.js
+++ src/js/generator/html-generator.js
@@ -1,26 +1,26 @@
 class HtmlGenerator {
     static generateHtmlElement(elementType, attributes = {}) {
-        const div = document.createElement(elementType);
-
-        const allowedAttributes = ['id', 'class', 'src', 'alt', 'data-src', 'data-bs-toggle', 'data-bs-target', 'aria-expanded', 'aria-controls', 'aria-labelledby', 'data-bs-parent' ];
+        const element = document.createElement(elementType);
 
         // Setting attributes
+        const allowedAttributes = ['id', 'class', 'src', 'alt', 'data-src', 'data-bs-toggle', 'data-bs-target', 'aria-expanded', 'aria-controls', 'aria-labelledby', 'data-bs-parent' ];
+
         for(let attribute in attributes) {
             if(allowedAttributes.includes(attribute)) {
-                div.setAttribute(attribute, attributes[attribute]);
+                element.setAttribute(attribute, attributes[attribute]);
             }
         }
 
         // Setting text and/or html
         if(attributes.text !== undefined && attributes.text.length) {
-            div.innerText = attributes.text;
+            element.innerText = attributes.text;
         }
 
         if(attributes.html !== undefined && attributes.html.length) {
-            div.innerHTML = attributes.html;
+            element.innerHTML = attributes.html;
         }
 
-        return div;
+        return element;
     }
 }
 
